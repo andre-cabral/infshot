97abb018447650b176c0bec6f7cd9a403e672951
andre-cabral <cabral.dev@gmail.com>
2015-11-08 21:35:57 -0200

    First commit. Manual weapon with kickback and return to position, controlled jump without first impulse, one-way platform working.

---
+++ Assets/Scripts/Player/GroundedCheck.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundedCheck : MonoBehaviour {
+
+	public PlayerMovement playerMovement;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.tag == Tags.platform){
+			if(playerMovement.getRigidbody2D().velocity.y <=0){
+				playerMovement.setGrounded(true);
+			}
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if(other.tag == Tags.platform){
+			if(playerMovement.getRigidbody2D().velocity.y >0){
+				playerMovement.setGrounded(false);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Player/WeaponShootAndKickback.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponShootAndKickback : MonoBehaviour {
+
+	public float minimumRotation = 270f;
+	public float maximumRotation = 359.9f;
+	public float kickBackPerShoot = 10f;
+	public float velocityToReturn = 80f;
+	public float timeToStartReturn = 0.2f;
+	float timeToStartReturnPassed = 0f;
+	bool returning = true;
+	public GameObject bullet;
+	public Transform bulletPositionObject;
+
+	void Update () {
+		if(Input.GetButton("Fire1")){
+			ShootBullet();
+		}
+
+		if(Input.GetButtonDown("Fire2")){
+			ShootBullet();
+		}
+
+		if(!returning){
+			if(timeToStartReturnPassed < timeToStartReturn){
+				timeToStartReturnPassed += Time.deltaTime;
+			}else{
+				timeToStartReturnPassed = 0f;
+				returning = true;
+			}
+		}
+
+		if(returning && transform.rotation.eulerAngles.z > minimumRotation){
+			WeaponDown();
+		}
+	}
+
+	void WeaponDown(){
+		if(transform.rotation.eulerAngles.z - Time.deltaTime*velocityToReturn > minimumRotation){
+			transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z - Time.deltaTime*velocityToReturn);
+		}else{
+			transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, minimumRotation);
+		}
+	}
+
+	public void ShootBullet(){
+		returning = false;
+		timeToStartReturnPassed = 0f;
+		Instantiate(bullet, bulletPositionObject.position, transform.rotation);
+		if(transform.rotation.eulerAngles.z + kickBackPerShoot <= maximumRotation){
+			transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z + kickBackPerShoot);
+		}else{
+			transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, maximumRotation);
+		}
+	}
+}
